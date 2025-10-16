5bdf3506971c4175e6442c479b62176434b21924
voxxus <98050377+Overo3@users.noreply.github.com>
2025-10-16 15:16:52 -0400

    evil wheel

---
+++ src/main/java/frc/team5115/subsystems/intake-wheel/IntakeWheelIOSparkMax.java
@@ -0,0 +1,47 @@
+package frc.team5115.subsystems.intakewheel;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import frc.team5115.Constants;
+import java.util.ArrayList;
+
+public class IntakeWheelIOSparkMax implements IntakeWheelIO {
+    private final SparkMax motor;
+    private final RelativeEncoder encoder;
+
+    public IntakeWheelIOSparkMax() {
+        motor = new SparkMax(Constants.INTAKE_MOTOR_ID, MotorType.kBrushless);
+        encoder = motor.getEncoder();
+
+        final SparkMaxConfig motorConfig = new SparkMaxConfig();
+        motorConfig.smartCurrentLimit(30, 40).idleMode(IdleMode.kCoast);
+        motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void updateInputs(IntakeWheelIOInputs inputs) {
+        inputs.velocityRPM = encoder.getVelocity();
+        inputs.appliedVolts = motor.getAppliedOutput() * motor.getBusVoltage();
+        inputs.currentAmps = motor.getOutputCurrent();
+    }
+
+    @Override
+    public void setPercent(double percent) {
+        motor.set(percent);
+    }
+
+    @Override
+    public void setVoltage(double volts) {
+        motor.setVoltage(volts);
+    }
+
+    @Override
+    public void getSparks(ArrayList<SparkMax> sparks) {
+        sparks.add(motor);
+    }
+}
