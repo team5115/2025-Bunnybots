10c02e287b470b6c2cdd5704df7bbb1e7d60a737
avidcoder27 <gilireeves@gmail.com>
2025-10-22 14:51:27 -0400

    synchro

--- src/main/java/frc/team5115/RobotContainer.java
+++ src/main/java/frc/team5115/RobotContainer.java
@@ -69,6 +69,7 @@ public class RobotContainer {
     private boolean slowMode = false;
     private boolean hasFaults = true;
     private double faultPrintTimeout = 0;
+    private boolean intakeMode = false;
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
