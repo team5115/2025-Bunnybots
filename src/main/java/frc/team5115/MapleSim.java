874db638b4217016b001297a6477be0426a598fd
avidcoder27 <gilireeves@gmail.com>
2025-11-17 16:22:24 -0500

    MapleSim swerve

--- src/main/java/frc/team5115/MapleSim.java
+++ src/main/java/frc/team5115/MapleSim.java
@@ -1,14 +1,19 @@
 package frc.team5115;
 
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
-
 import org.ironmaple.simulation.SimulatedArena;
-import org.ironmaple.simulation.seasonspecific.crescendo2024.CrescendoNoteOnField;
+import org.ironmaple.simulation.drivesims.GyroSimulation;
+import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
+import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
+import org.ironmaple.simulation.seasonspecific.crescendo2024.CrescendoNoteOnField;
 
 public class MapleSim {
-    private final DriveTrainSimulationConfig driveTrainSimulationConfig;
 
     public static SimulatedArena getInstance() {
         return SimulatedArena.getInstance();
@@ -22,24 +27,33 @@ public class MapleSim {
         SimulatedArena.getInstance().simulationPeriodic();
     }
 
-    public static void setupDriveSim() {
-        driveTrainSimulationConfig.DriveTrainSimulationConfig.Default()
-        // Specify gyro type (for realistic gyro drifting and error simulation)
-        .withGyro(COTS.ofPigeon2())
-        // Specify swerve module (for realistic swerve dynamics)
-        .withSwerveModule(new SwerveModuleSimulationConfig(
-                DCMotor.getKrakenX60(1), // Drive motor is a Kraken X60
-                DCMotor.getFalcon500(1), // Steer motor is a Falcon 500
-                6.12, // Drive motor gear ratio.
-                12.8, // Steer motor gear ratio.
-                Volts.of(0.1), // Drive friction voltage.
-                Volts.of(0.1), // Steer friction voltage
-                Inches.of(2), // Wheel radius
-                KilogramSquareMeters.of(0.03), // Steer MOI
-                1.2)) // Wheel COF
-        // Configures the track length and track width (spacing between swerve modules)
-        .withTrackLengthTrackWidth(Inches.of(24), Inches.of(24))
-        // Configures the bumper size (dimensions of the robot bumper)
-        .withBumperSize(Inches.of(30), Inches.of(30));
+    public static DriveTrainSimulationConfig getDriveSimConfig() {
+        return DriveTrainSimulationConfig.Default()
+                // Specify gyro type (for realistic gyro drifting and error simulation)
+                .withGyro(() -> new GyroSimulation(0, 0))
+                // Specify swerve module (for realistic swerve dynamics)
+                .withSwerveModule(
+                        new SwerveModuleSimulationConfig(
+                                DCMotor.getNEO(1), // Drive motor is a NEO
+                                DCMotor.getNeo550(1), // Steer motor is a NEO 550
+                                5.007, // Drive motor gear ratio.
+                                12, // Steer motor gear ratio.
+                                Volts.of(0.1), // Drive friction voltage.
+                                Volts.of(0.1), // Steer friction voltage
+                                Inches.of(2), // Wheel radius
+                                KilogramSquareMeters.of(0.03), // Steer MOI
+                                1.2)) // Wheel COF
+                // Configures the track length and track width (spacing between swerve modules)
+                .withTrackLengthTrackWidth(Inches.of(24), Inches.of(24))
+                // Configures the bumper size (dimensions of the robot bumper)
+                .withBumperSize(Inches.of(30), Inches.of(30)); // TODO: correct numbers
+    }
+
+    public static SwerveDriveSimulation initInstance() {
+        return new SwerveDriveSimulation(
+                // Specify Configuration
+                MapleSim.getDriveSimConfig(),
+                // Specify starting pose
+                new Pose2d(3, 3, new Rotation2d()));
     }
 }
